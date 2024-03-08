b8279dc76e836f8f06018746e27a66288287601e
jungsun <dlwjdtjs0111@gmail.com>
2024-03-08 15:05:15 +0900

    이정선 2022111942

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 #Git 실습하기
 name = input("당신의 이름을 입력하세요 : ")
 number = input("당신의 학번을 입력하세요 : ")
+department = input("당신의 학과를 입력하세요 : ")
 print("===================================")
 print("이름 : ", name)
 print("학번 : ", number)
+print("학과 : ", department)
